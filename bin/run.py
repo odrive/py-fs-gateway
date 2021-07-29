cfc52846e9084b330406794dd1fa4ee04544f101
Tony <amagliul@gmail.com>
2021-07-29 08:50:15 -0700

    expand and creation of handler.v2.metadata.temp.dir

--- bin/run.py
+++ bin/run.py
@@ -16,6 +16,7 @@ with open(os.path.join(os.getcwd(), 'config.json'), 'r') as data_file:
 # Convert relative paths to absolute paths.
 config['wsgi.log.path'] = os.path.abspath(os.path.expanduser(config['wsgi.log.path']))
 config['controller.datastore.path'] = os.path.abspath(os.path.expanduser(config['controller.datastore.path']))
+config['handler.v2.metadata.temp.dir'] = os.path.abspath(os.path.expanduser(config['handler.v2.metadata.temp.dir']))
 config['handler.v2.metadata_file.temp.dir'] = os.path.abspath(os.path.expanduser(config['handler.v2.metadata_file.temp.dir']))
 config['handler.v2.file_thumbnail.temp.dir'] = os.path.abspath(os.path.expanduser(config['handler.v2.file_thumbnail.temp.dir']))
 config['handler.v2.auth.acl.path'] = os.path.abspath(os.path.expanduser(config['handler.v2.auth.acl.path']))
@@ -23,6 +24,8 @@ config['handler.v2.auth.acl.path'] = os.path.abspath(os.path.expanduser(config['
 # Ensure folder ready.
 if not os.path.exists(config['controller.datastore.path']):
     os.makedirs(config['controller.datastore.path'])
+if not os.path.exists(config['handler.v2.metadata.temp.dir']):
+    os.makedirs(config['handler.v2.metadata.temp.dir'])
 if not os.path.exists(config['handler.v2.file_thumbnail.temp.dir']):
     os.makedirs(config['handler.v2.file_thumbnail.temp.dir'])
 if not os.path.exists(config['handler.v2.metadata_file.temp.dir']):
